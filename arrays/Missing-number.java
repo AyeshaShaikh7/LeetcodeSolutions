4b85d0c9c3e811b02a1512711c52b84734ed486c
Ayesha Shaikh <ayeshansk7@gmail.com>
2026-03-15 22:52:48 +0530

    Add Missing number solution in java

--- arrays/Missing-number.java
+++ arrays/Missing-number.java
@@ -4,7 +4,7 @@
 // Difficulty : easy
 
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber(int[] nums) { 
         int n= nums.length; int sum=0; int sum1=0;
         for(int i=0;i<n;i++){
              sum = sum + nums[i];
