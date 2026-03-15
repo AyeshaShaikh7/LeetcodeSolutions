5a1cd8b405ac551fb07e2d0383054da90d74bc17
Ayesha Shaikh <ayeshansk7@gmail.com>
2026-03-15 16:39:48 +0530

    Implement solution for max profit, buy and sell problem

---
+++ arrays/Buy-and-Stock.java
@@ -0,0 +1,19 @@
+// Problem : Best Time to Buy and Sell Stock
+// Platform : Leetcode
+// Topic : Arrays, Dynamic Programming
+// Difficulty : Easy
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit =0; int min = prices[0];
+        for(int i=0;i<prices.length;i++){
+             int profitoday = prices[i] - min; //profit = SP-CP;
+            if(profitoday>profit){
+                profit = profitoday;
+            }
+           if(prices[i]<min){
+            min = prices[i];
+           } 
+
+        } return profit;
+    }
+}
